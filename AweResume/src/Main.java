dd8ab9549f1fc029126b9b94d360abb46566fb47
Nargiz Shahgeldi <nargizshahgeldiyeva@gmail.com>
2026-04-29 19:43:38 +0400

    Commit by Araz

--- AweResume/src/Main.java
+++ AweResume/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("I dont't know what to write");
         System.out.println("Nara made some changes");
+        System.out.println("Araz changed his project one more time");
     }
 }
